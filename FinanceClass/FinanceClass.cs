9015e826b1dd5c8d96771293a42acbe0e376451d
JD Marymee <jmarymee@microsoft.com>
2016-12-16 13:49:07 -0800

    Initial Commit. Includes payment class

---
+++ FinanceClass/FinanceClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceClass
+{
+    /// <summary>
+    /// Class to calculate loan payments
+    /// </summary>
+    public class PaymentCalculator
+    {
+        private const int MonthsPerYear = 12;
+
+        /// <summary>
+        /// The total purchase price of the item being paid for.
+        /// </summary>
+        public decimal PurchasePrice { get; set; }
+
+        /// <summary>
+        /// The total down payment towards the item being purchased.
+        /// </summary>
+        public decimal DownPayment { get; set; }
+
+        /// <summary>
+        /// Gets the total loan amount. This is the purchase price less
+        /// any down payment.
+        /// </summary>
+        public decimal LoanAmount
+        {
+            get { return (PurchasePrice - DownPayment); }
+        }
+
+        /// <summary>
+        /// The annual interest rate to be charged on the loan
+        /// </summary>
+        public double InterestRate { get; set; }
+
+        /// <summary>
+        /// The term of the loan in months. This is the number of months
+        /// that payments will be made.
+        /// </summary>
+        public double LoanTermMonths { get; set; }
+
+        /// <summary>
+        /// The term of the loan in years. This is the number of years
+        /// that payments will be made.
+        /// </summary>
+        public double LoanTermYears
+        {
+            get { return LoanTermMonths / MonthsPerYear; }
+            set { LoanTermMonths = (value * MonthsPerYear); }
+        }
+
+        /// <summary>
+        /// Calculates the monthy payment amount based on current
+        /// settings.
+        /// </summary>
+        /// <returns>Returns the monthly payment amount</returns>
+        public decimal CalculatePayment()
+        {
+            decimal payment = 0;
+
+            if (LoanTermMonths > 0)
+            {
+                if (InterestRate != 0)
+                {
+                    double rate = ((InterestRate / MonthsPerYear) / 100);
+                    double factor = (rate + (rate / (Math.Pow(rate + 1, LoanTermMonths) - 1)));
+                    payment = (LoanAmount * (decimal)factor);
+                }
+                else payment = (LoanAmount / (decimal)LoanTermMonths);
+            }
+            return Math.Round(payment, 2);
+        }
+    }
+
+}
---
+++ FinanceConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinanceClass;
+
+namespace FinanceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentCalculator calculator = new PaymentCalculator();
+            calculator.PurchasePrice = 50000;
+            calculator.InterestRate = 6.0;
+            calculator.LoanTermYears = 5;
+            for (int i = 0; i <= 10000; i += 1000)
+            {
+                calculator.DownPayment = i;
+                DisplayLoanInformation(calculator);
+            }
+
+            Console.WriteLine("Press any to end");
+            Console.ReadLine();
+
+        }
+
+        static void DisplayLoanInformation(PaymentCalculator calculator)
+        {
+            Console.WriteLine("Purchase Price: {0:C}", calculator.PurchasePrice);
+            Console.WriteLine("Down Payment: {0:C}", calculator.DownPayment);
+            Console.WriteLine("Loan Amount: {0:C}", calculator.LoanAmount);
+            Console.WriteLine("Annual Interest Rate: {0}%", calculator.InterestRate);
+            Console.WriteLine("Term: {0} years ({1} months)", calculator.LoanTermYears, calculator.LoanTermMonths);
+            Console.WriteLine("Monthly Payment: {0:C}", calculator.CalculatePayment());
+            Console.WriteLine();
+        }
+    }
+}
